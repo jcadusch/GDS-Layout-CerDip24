91dd4548c8f2ae9149f8b94d76e68647396778f4
Jasper Cadusch <jcadusch@gmail.com>
2017-09-01 17:21:17 +1000

    Add files via upload

--- wg_layout.py
+++ wg_layout.py
@@ -7,64 +7,97 @@
 ###-------------------------#####
 
 ## dependencies
-from IPython import get_ipython
-get_ipython().magic('reset -sf') ## when using IDE (eg spyder)
- 
 
-import matplotlib.pyplot as plt
+#from IPython import get_ipython
+#get_ipython().magic('reset -sf') ## when using IDE (eg spyder)
 import numpy as np
-import gdspy
 from gdsCAD import *
 
-## Parameters
-TopCell=core.Cell('TOP')
+##-- Parameters --##
+    
+    ### Waveguide Parameters 
 L = 300.0 # waveguide length in um
-w = 0.1 # waveguide width in um
-PadLen = 200.0 # pad size side length in um
-shelf = 200.0 # pad shelf size side length in um
-MesaLen = 420.0 # mesa size in um
-MesaWid = 240.0
-MesaOffset = 5.0
+thin = 0.03 ## minimum waveguide width
+thick = 0.145 ## maximum waveguide width
+step = 0.005 ## waveguide width increment
+tck = np.arange(thin,thick,step) ##---- waveguide widths [replace with 23 width values from experiment] ----## 
 period = 0.5 # wg array period in um
-nWg = int(shelf/period) # number of waveguides
+
+    ### Pad Parameters
+PadLen = 200.0 # pad size side length in um
 PadSpace = 750.0 #  pad spacing centre to centre in um
 overlap = 0.5 # pad-detector overlap in um
+nWg = int(PadLen/(period*np.sqrt(2))) # number of waveguides
+
+
+    ### Mesa Parameters
+MesaLen = 420.0 # mesa size in um
+MesaWid = 240.0 # mesa width for pad region
+MesaOffset = 5.0 # mesa overhang for waveguide region
+
+    ### Alignment Markers Parameters
 AlignMx = 3000.0 # alignment marker x position
 AlignMy = AlignMx # alignment marker y position
 AlignMw = 20.0 # alignment marker width
 AlignMl = 100.0 # alignment marker length
 
-thin = 0.03 ## minimum waveguide width
-thick = 0.145 ## maximum waveguide width
-step = 0.005 ## waveguide width increment
+##-- End Parameters --##
 
-tck = np.arange(thin,thick,step) ##---- replace with 23 width values from experiment ----## 
 
-## create 23 photodiode positions
+## create 23 photodiode positions for 24 pin chip carrier (pin 3 used for back contact)
 x = PadSpace*np.array([-7/2,-7/2,-5/2,-3/2,-1/2,1/2,3/2,5/2,7/2,7/2,7/2,7/2,7/2,7/2,5/2,3/2,1/2,-1/2,-3/2,-5/2,-7/2,-7/2,-7/2])
 y = PadSpace*np.array([-1/2,-3/2,-7/2,-7/2,-7/2,-7/2,-7/2,-7/2,-5/2,-3/2,-1/2,1/2,3/2,5/2,7/2,7/2,7/2,7/2,7/2,7/2,5/2,3/2,1/2])
+AngList = np.array([0.,0.,90.,90.,90.,90.,90.,90.,180.,180.,180.,180.,180.,180.,270.,270.,270.,270.,270.,270.,0.,0.,0.])  ## pad direction list
 
-AngList = np.array([0,0,90,90,90,90,90,90,180,180,180,180,180,180,270,270,270,270,270,270,0,0,0])
+pin = 1 ## a counter
 
-#### Create Fins ####
+TopCell=core.Cell('TOP') ## create the main cell
 
-#fin = shapes.Rectangle((-0.5*w,-0.5*L), (0.5*w,0.5*L), layer=2)
-#
-#wgcell = core.Cell('wg')
-#
-#wgcell.add(fin)
-#
-#wgcellarray = core.CellArray(wgcell,nWg,1,(period,1))
-#
-#FinCell = core.Cell('Fins')
-#
-#FinCell.add(wgcellarray)
+for (posx,posy,ang,w) in zip(x,y,AngList,tck):
+    wgcell = core.Cell('wg'+repr(pin))
+    
+    wgcell2 = core.Cell('wg_2_'+repr(pin))
+    
+    #### Create Fins for Detector 1 ####
+
+    fin = shapes.Rectangle((-0.5*L,-0.5*w), (0.5*L,0.5*w), layer=4)
+              
+    fin.rotate(-45)
+        
+    fin.translate((posx+PadLen/2+(L+w)/np.sqrt(8)-overlap/np.sqrt(2),posy-(L-w)/np.sqrt(8)-PadLen/2+overlap/np.sqrt(2)))
+    
+    fin.rotate(ang,(posx,posy))
+    
+    wgcell.add(fin)
+    
+    wgcellarray = core.CellArray(wgcell,nWg,1,((period*np.cos(np.deg2rad(ang+45)), period*np.sin(np.deg2rad(ang+45))),(0,0)))
+    
+    TopCell.add(wgcellarray)
+            
+    del wgcell
+    
+    ### Fins for Detector 2 ####
+    
+    fin2 = shapes.Rectangle((-0.5*L,-0.5*w), (0.5*L,0.5*w), layer=4)
+              
+    fin2.rotate(45)
+        
+    fin2.translate((posx+PadLen/2+(L+w)/np.sqrt(8)-overlap/np.sqrt(2),posy+(L-w)/np.sqrt(8)+PadLen/2-overlap/np.sqrt(2)))
+    
+    fin2.rotate(ang,(posx,posy))
+    
+    wgcell2.add(fin2)
+    
+    wgcellarray2 = core.CellArray(wgcell2,nWg,1,((period*np.cos(np.deg2rad(ang-45)), period*np.sin(np.deg2rad(ang-45))),(0,0)))
+    
+    TopCell.add(wgcellarray2)
+    
+    del wgcell2
+    
+    pin += 1
 
 
-
-#### Create Mesa ####
-
-for (posx,posy,ang) in zip(x,y,AngList):
+    #### Create Mesa ####
 
         
     mesa1 = shapes.Rectangle((-0.5*MesaLen+PadLen/4,-0.5*MesaWid), (0.5*MesaLen+PadLen/4,0.5*MesaWid), layer=3)
@@ -91,12 +124,8 @@ for (posx,posy,ang) in zip(x,y,AngList):
     
     TopCell.add([mesa1,mesa2,mesa3])
 
-#### Create Pads ####
+    #### Create Pads ####
 
-
-
-
-        
     pad = shapes.Rectangle((-0.5*PadLen,-0.5*PadLen), (0.5*PadLen,0.5*PadLen), layer=1)
 
     pad.translate((posx,posy))
@@ -112,6 +141,7 @@ for (posx,posy,ang) in zip(x,y,AngList):
     tri.rotate(ang,(posx,posy))
     
     TopCell.add(tri)
+        
 #### Create Markers ####
 
 label = shapes.LineLabel('', 70,(x[0]-2*PadLen,y[0]),layer=2)
@@ -131,10 +161,8 @@ label4.add_text('Pin 18','romant')
 TopCell.add(label4)
 
 #### Add cells to layout ####
-
 Wg_layout = core.Layout('WgLib')
 Wg_layout.add(TopCell)
 
-### Save GDS ###
-Wg_layout.save('../GDSII/wg/wg_chip.gds')
-#FN_layout.save('../GDSII/fishnet/fishnet_chip.gds')
+### Save GDS layout ###
+Wg_layout.save('./WG_v02/wg_chip.gds') ### set your own path/filename here
